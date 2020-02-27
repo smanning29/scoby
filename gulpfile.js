9008d54564045db7dc0320f2a6c8708834cbe0cb
Sarah <sama3207@colorado.edu>
2020-02-27 15:46:32 -0700

    added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,35 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
 
 function style() {
-    return gulp.src('./scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+return gulp.src('./scss/**/*.scss')
+.pipe(sourcemaps.init())
+//default style indented based on nesting
+.pipe(sass({outputStyle: 'nested'}))
+//indented but not based on nesting
+// .pipe(sass({outputStyle: 'expanded'}))
+//rules are one line
+// .pipe(sass({outputStyle: 'compact'}))
+//production css (minified) one line no white space
+// .pipe(sass({outputStyle: 'compressed'}))
+.pipe(browserSync.stream())
+.pipe(autoprefixer({
+    cascade: false
+}))
+.pipe(sourcemaps.write('.'))
+.pipe(gulp.dest('./css'));
 }
 
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+function watch() { browserSync.init({
+server: {
+baseDir: './' }
+});
+gulp.watch('./scss/**/*.scss', {ignoreInitial: false}, style);
+gulp.watch('./*.html').on('change', browserSync.reload);
+gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
