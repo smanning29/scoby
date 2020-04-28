17a7a87913ea764f851ed803d868f0b609118390
Sarah <sama3207@colorado.edu>
2020-04-27 18:16:24 -0700

    html works

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
@@ -15,24 +14,6 @@ gulp.task('css', function () {
       .pipe(browserSync.stream())
       .pipe(gulp.dest('./dest/css'));
   });
-=======
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-function style() {
-return gulp.src('./scss/**/*.scss')
-//default style indented based on nesting
-.pipe(sass({outputStyle: 'nested'}))
-//indented but not based on nesting
-// .pipe(sass({outputStyle: 'expanded'}))
-//rules are one line
-// .pipe(sass({outputStyle: 'compact'}))
-//production css (minified) one line no white space
-// .pipe(sass({outputStyle: 'compressed'}))
-.pipe(browserSync.stream())
-.pipe(gulp.dest('./css'));
-}
->>>>>>> d3ca32e868e30d9d0f738e8a8e2558968f25f836
 
   function watch() { browserSync.init({
     server: {
